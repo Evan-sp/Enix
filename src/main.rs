77f6e19f2d251f7eb0a72ceac2e7de3f363f04ab
Evan-sp <epspensley@gmail.com>
2024-10-22 17:21:56 -0400

    Execute files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::io::{self, Write};
+use std::fs;
+use std::path::Path;
+use std::os::unix::fs::PermissionsExt;
+use std::process::Command;
 
 fn main() {
     loop {
@@ -23,5 +27,47 @@ fn main() {
 }
 
 fn parse(input: &str) {
-    println!("Parsing: {}", input);
+    let words: Vec<&str> = input.split_whitespace().collect();
+    let first_word = match words.get(0) {
+        Some(word) => word,
+        None => "No words found",
+    };
+    launch(first_word);
 }
+
+fn launch(command: &str) {
+    let path = Path::new(command);
+    if !path.exists() {
+        println!("File {} does not exist", command);
+        return;
+    }
+    if !path.is_file() {
+        println!("'{}' is not a file", command);
+        return;
+    }
+    
+    let metadata = fs::metadata(path).unwrap();
+    let permissions = metadata.permissions();
+    let mode = permissions.mode();
+    let user_read = mode & 0o400 != 0;
+    let user_write = mode & 0o200 != 0;
+    let user_exec = mode & 0o100 != 0;
+
+    println!("File '{}':", command);
+    println!("  Read permission: {}", user_read);
+    println!("  Write permission: {}", user_write);
+    println!("  Execute permission: {}", user_exec);
+    
+    let output = Command::new(path)
+        .output()
+        .expect("Failed to execute the command");
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        println!("{}", stdout.trim());
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Error output:\n {}", stderr);
+    }
+}
+
